f3c8ef89e74773651d7c84c8895b59b1fa968ace
Tomasz Stabinski <Tomasz.Stabinski@gmail.com>
2013-07-10 20:16:21 +0000

    Dodanie obslugi web.js

--- web.js
+++ web.js
@@ -1,12 +1,11 @@
-var express = require('express');
+var http = require('http');
+var fs = require('fs');
+var sys = require('sys');
 
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+http.createServer(function(req, res){
+    fs.readFile('index.html',function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        res.write(data);
+        res.end();
+    });
+}).listen(5000);
