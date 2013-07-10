dc1721ecf52d9d3e2bb11e52ff5bd2afabd15f22
Tomasz Stabinski <Tomasz.Stabinski@gmail.com>
2013-07-10 21:02:52 +0000

    Naprawde finalny kod

--- web.js
+++ web.js
@@ -1,14 +1,17 @@
-var http = require('http'),
-    fs = require('fs');
+var fs = require('fs');
+var express = require('express');
 
+var text = "";
 
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(5000);
+var app = express.createServer(express.logger());
+
+app.get('/', function(request, res) {
+    fs.readFile('./index.html', 'utf8', function(err, text){
+        res.send(text);
+    });
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
